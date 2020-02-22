24cd30b5d0953563fa9d4b634078126ecf0ad308
Simo Khouili <m.simokhouili@gmail.com>
2020-02-22 15:13:31 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,16 @@
 
 def key_for_min_value(name_hash)
 
+  min_value = 0
+  lower_key = nil
+
+  name_hash.each do |key, value|
+    if min_value == 0 || value < min_value
+
+      min_value = value
+      lower_key = key
+
+    end
+  end
+    lower_key
 end
