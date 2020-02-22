e6ea5327a42b5ed588a4eb64c70c8168930ae083
Simo Khouili <m.simokhouili@gmail.com>
2020-02-22 15:02:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,9 +8,10 @@ def key_for_min_value(name_hash)
 
   name_hash.each do |key, value|
     if min_value == 0 || value < min_value
-      binding.pry
+
       value = min_value
       lower_key = key
+      binding.pry
     end
   end
 end
